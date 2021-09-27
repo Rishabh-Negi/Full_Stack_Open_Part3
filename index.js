a2bd2133327d18109fc90e034e00be7f7deef546
Rishabh_Negi <nrishabh20@gmail.com>
2021-09-27 16:02:44 +0530

    part3-phonebook-3.9

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
+app.use(cors())
 
 morgan.token('body', function (req, res) {
     return JSON.stringify(req.body)
